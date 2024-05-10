bdc308271fbc3a6e0ea445c1d1222768132b77b2
Mehak Kamran <mehakkamran52@gmail.com>
2024-05-11 00:11:22 +0500

    creating routes and views

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res) {
   res.render('index');
 });
 
+router.get('/register', function(req, res) {
+  res.render('register');
+});
+
+router.get('/login', function(req, res) {
+  res.render('login');
+});
+
 module.exports = router;
